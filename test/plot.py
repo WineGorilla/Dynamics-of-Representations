4bd853c467c058cc627d8b82c63e9d63c2f09a80
WineGorilla <winegorilla2@gmail.com>
2026-03-29 20:50:34 -0400

    add the analysis assets

--- test/plot.py
+++ test/plot.py
@@ -1,3 +1,6 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.colors import LogNorm
